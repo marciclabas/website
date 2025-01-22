bdda49b128ea374ed158ebb71efc8731212eceeb
m4rs <marcel@marcelclaramunt.cat>
2025-01-22 22:24:33 +0100

    rm old site

--- src/lib/NotionPage.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { NotionRenderer } from 'react-notion-x'
-
-export type Props = Parameters<typeof NotionRenderer>[0]
-export function Renderer(props: Props) {
-  return <NotionRenderer darkMode={true} {...props} />
-}
-
-export default Renderer
--- src/lib/notion.ts
+++
@@ -1,4 +0,0 @@
-// lib/notion.ts
-import { NotionAPI } from 'notion-client';
-
-export const notion = new NotionAPI();
